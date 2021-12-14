ca7152e6c5fb8825771c9062c4758468ce50035c
rachel <rachelfox777@gmail.com>
2021-12-14 22:42:44 +0000

    worked on files

---
+++ scripts.js
@@ -0,0 +1,480 @@
+
+
+currentYear = 1978;
+
+
+document.querySelector("#scrollUp").onclick = function() {
+  
+  gotoTop = 0;
+  
+  if (currentYear == 1979) gotoTop = 0;
+  else if (currentYear == 1980) gotoTop = 100;
+ 
+  
+   document.querySelector("#dates").scroll({
+    top: gotoTop,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+}
+
+
+
+document.querySelector("#year1978").onclick = function() {
+  document.querySelector("#issues").scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+  
+
+
+
+document.querySelector("#year1979").onclick = function() {
+  currentYear = 1979;
+  
+  document.querySelector("#issues").scroll({
+    top: 600,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 100,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+}
+
+
+
+
+
+
+document.querySelector("#year1980").onclick = function() {
+  currentYear =1980; 
+    
+  document.querySelector("#issues").scroll({
+    top: 1200,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 200,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year1984").onclick = function() {
+  currentYear = 1984;
+    
+  document.querySelector("#issues").scroll({
+    top: 1800,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 300,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year1987").onclick = function() {
+  currentYear = 1987;
+    
+  document.querySelector("#issues").scroll({
+    top: 2400,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 400,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year1991").onclick = function() {
+  currentYear = 1991;
+ 
+    document.querySelector("#issues").scroll({
+    top: 3000,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 500,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year1994").onclick = function() {
+  currentYear = 1994;
+  
+  document.querySelector("#issues").scroll({
+    top: 3600,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 600,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+  
+document.querySelector("#year1995").onclick = function() {
+  currentYear = 1995;
+  
+  document.querySelector("#issues").scroll({
+    top: 4200,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 700,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year1997").onclick = function() {
+  currentYear = 1997;
+  
+  document.querySelector("#issues").scroll({
+    top: 4800,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 800,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2006").onclick = function() {
+  currentYear = 2006;
+  
+  document.querySelector("#issues").scroll({
+    top: 5400,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 900,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2007").onclick = function() {
+  currentYear = 2007;
+  
+  document.querySelector("#issues").scroll({
+    top: 6000,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 1000,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2009").onclick = function() {
+  currentYear = 2009;
+  
+  document.querySelector("#issues").scroll({
+    top: 6600,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 1100,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2010").onclick = function() {
+  currentYear = 2010;
+  
+  document.querySelector("#issues").scroll({
+    top: 7200,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 1200,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+  
+document.querySelector("#year2011").onclick = function() {
+  currentYear = 2011;
+  
+  document.querySelector("#issues").scroll({
+    top: 7800,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 1300,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2012").onclick = function() {
+  currentYear = 2012;
+  
+  document.querySelector("#issues").scroll({
+    top: 8400,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 1400,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2013").onclick = function() {
+  currentYear = 2013;
+  
+  document.querySelector("#issues").scroll({
+    top: 9000,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 1500,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2014").onclick = function() {
+  currentYear = 2014;
+  
+  document.querySelector("#issues").scroll({
+    top: 9600,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 1600,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2015").onclick = function() {
+  currentYear = 2015;
+  
+  document.querySelector("#issues").scroll({
+    top: 10200,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 1700,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2016").onclick = function() {
+  currentYear = 2016;
+  
+  document.querySelector("#issues").scroll({
+    top: 10800,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 1800,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+  
+document.querySelector("#year2017").onclick = function() {
+  currentYear = 2017;
+  
+  document.querySelector("#issues").scroll({
+    top: 11400,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 1900,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2018").onclick = function() {
+  
+  currentYear = 2018;
+  
+  document.querySelector("#issues").scroll({
+    top: 12000,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 2000,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2019").onclick = function() {
+  currentYear = 2019;
+  
+  document.querySelector("#issues").scroll({
+    top: 12600,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 2100,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+document.querySelector("#year2021").onclick = function() {
+  currentYear = 2021;
+
+    document.querySelector("#issues").scroll({
+    top: 13200,
+    left: 0,
+    behavior: 'smooth'
+  });
+  
+  document.querySelector("#dates").scroll({
+    top: 2200,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
+
+
+
+
